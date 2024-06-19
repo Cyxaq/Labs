bdc3f6a2594d7f4ef8a91def7712d11525370c94
Yahor Sukhanau (279452) <279452@student.pwr.edu.pl>
2024-06-19 14:18:04 +0200

    done

--- Lab12/Szyfrowanie DES/main.py
+++ Lab12/Szyfrowanie DES/main.py
@@ -27,3 +27,14 @@ key_filename = filename.replace('.txt', '.key')
 keys_info = f"Initialization vector:{ivec} \nKey:{key}"
 with open("files/keys/"+key_filename, 'w') as file:
     file.write(keys_info)
+
+"""
+project zawiera folder "files" w którym:
+    folder "plaintext" sluży dla przechowywania plików z tekstem niezaszyfrowanym
+    folder "encrypted" - przechowywanie tych samych plików zaszyfrowanych
+    folder "keys" - do szyfrowania wykorzystane podany klucz i IV
+    
+    Np. "plaintext/test1.txt" się zaszyfruje za pomocą IV i key z plika "keys/test1.key". Wynik będzie w "encrypted/test1.enc"
+    
+    za każdym razem jak szfrujemy dane klucz sie generuje ramdomowo (może cały czas się zmieniać)
+"""
