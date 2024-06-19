5a5841d34686a1cabeb15a182ed0239f5a142f8b
Yahor Sukhanau (279452) <279452@student.pwr.edu.pl>
2024-06-19 15:20:55 +0200

    Done

--- Lab12/Podpisywanie cyfrowe/main.py
+++ Lab12/Podpisywanie cyfrowe/main.py
@@ -4,9 +4,25 @@ from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
 
 key = RSA.generate(2048)
-private_key = key.export_key()
-public_key = key.publickey().export_key()
-# print(key)
-# print(private_key)
-# print(public_key)
+private_key = key
+public_key = key.publickey()
 
+
+
+filenameToSign = str(input("Wprowadź nazwę plika do podpisania: "))
+
+with open("files/keys/Public/"+(filenameToSign.replace('.txt', '.key')), 'w') as file:
+    file.write(str(public_key.export_key()))
+
+with open("files/keys/Private/"+(filenameToSign.replace('.txt', '.key')), 'w') as file:
+    file.write(str(private_key.export_key()))
+
+
+with open("files/UnSigned/"+filenameToSign, 'rb') as file:
+    txt = file.read()
+hash = SHA256.new(txt)
+sign = pkcs1_15.new(private_key).sign(hash)
+
+signedFilename = filenameToSign+'.sig'
+with open("files/Signed/"+signedFilename, 'bw') as file:
+    file.write(sign)
